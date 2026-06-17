01c862ea576840ce98ab602d9f731ab8140a9f09
Sam Raymer <sam@sam-raymer.com>
2026-06-16 23:43:28 -0700

    Add: matrixBFS - submission-15

---
+++ Data Structures & Algorithms/matrixBFS/submission-15.py
@@ -0,0 +1,34 @@
+import collections
+class Solution:
+    
+    def shortestPath(self, grid: List[List[int]]) -> int:
+        yLen = len(grid)
+        xLen = len(grid[0])
+
+        toVisit: collections.deque[tuple[int, int, int]] = collections.deque() # x, y, depth
+        visited: Set[tuple[int, int]] = set()
+        if grid[0][0] == 1: return -1
+        toVisit.append((0,0,0))
+        visited.add((0,0))
+        pathLen = None
+
+        while pathLen == None and len(toVisit) > 0:
+            x, y, depth = toVisit.popleft()
+
+            if x == xLen - 1 and y == yLen - 1:
+                return depth
+            
+            if x > 0 and ((x - 1, y) not in visited) and grid[y][x-1] != 1:
+                toVisit.append((x - 1, y, depth + 1))
+                visited.add((x - 1, y))
+            if x < (xLen - 1) and ((x + 1, y) not in visited) and grid[y][x+1] != 1:
+                toVisit.append((x + 1, y, depth + 1))
+                visited.add((x + 1, y))
+            if y > 0 and ((x, y-1) not in visited) and grid[y-1][x] != 1:
+                toVisit.append((x, y-1, depth + 1))
+                visited.add((x, y - 1))
+            if y < (yLen - 1) and ((x, y+1) not in visited) and grid[y+1][x] != 1:
+                toVisit.append((x, y+1, depth + 1))
+                visited.add((x, y + 1))
+        if pathLen == None: return -1
+        else: return pathLen
