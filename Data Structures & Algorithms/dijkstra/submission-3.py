a777adfae46def5771c5aa257c5e9c0ae532fc07
Sam Raymer <sam@sam-raymer.com>
2026-06-17 22:27:01 -0700

    Add: dijkstra - submission-3

---
+++ Data Structures & Algorithms/dijkstra/submission-3.py
@@ -0,0 +1,30 @@
+from functools import total_ordering
+import heapq
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        toVisit: List[tuple[int, int]] = []
+        shortestByVertexId: Dict[int, int] = {}
+        graph: Dict[int, Set[tuple[int, int]]] = {}
+        for i in range(n):
+            shortestByVertexId[i] = -1
+            graph[i] = set()
+        for edge in edges:
+            source, destination, weight = edge
+            graph[source].add((destination, weight))
+        heapq.heappush(toVisit, (0, src))
+        # for source, edgeSet in graph.items():
+        #     print(edgeSet)
+
+        while len(toVisit) > 0:
+            edge = heapq.heappop(toVisit)
+            distance, destination = edge
+            # print("{u}, {v} - {w} left".format(u=destination, v=distance, w=toVisit.qsize()))
+            currentShortest = shortestByVertexId[destination]
+            if currentShortest == -1:
+                shortestByVertexId[destination] = distance
+            else:
+                continue
+            for nextDest, nextWeight in graph[destination]:
+                heapq.heappush(toVisit,(distance + nextWeight, nextDest))
+
+        return shortestByVertexId
