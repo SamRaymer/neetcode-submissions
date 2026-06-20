df94b608dda354992b974ef13b2d97dcade2247a
Sam Raymer <sam@sam-raymer.com>
2026-06-19 17:04:06 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def buildDict(strIn: str) -> Dict[str, int]:
+            result = {}
+            for curChar in strIn:
+                if curChar in result:
+                    print("wahoo")
+                    result[curChar] += 1
+                else:
+                    result[curChar] = 1
+            return result
+        sDict = buildDict(s)
+        tDict = buildDict(t)
+        for curChar, occurrences in sDict.items():
+            if curChar not in tDict: return False
+            if tDict[curChar] != occurrences: return False
+            tDict.pop(curChar)
+            
+        return True if len(tDict) == 0 else False
+
+
+
